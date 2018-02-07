40596d387c34b0d0f6e2c208df5e5682c7d4cf6f
P16187679 <P16187679@GWHXX681WIN105W>
2018-02-07 13:10:08 +0000

    Setting up the main doc

---
+++ CustomersTestFramework/tstCustomers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomersTestFramework
+{
+    [TestClass]
+    public class tstCustomers
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ OrdersTestFramework/tstOrders.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OrdersTestFramework
+{
+    [TestClass]
+    public class tstOrders
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
