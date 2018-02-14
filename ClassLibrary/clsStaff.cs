d78c22eaca3ec1bb5ec732d459c224faad0342e9
p1619094x <p1619094x@GWHXX282WIN013W>
2018-02-14 10:49:18 +0000

    Created test properties for clsStaff

---
+++ ClassLibrary/clsStaff.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsStaff
+    {
+        public string Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NextOfKin { get; set; }
+        public string PhoneNo { get; set; }
+        public string StaffCode { get; set; }
+
+        public bool Find(string staffCode)
+        {
+            //always return true
+            return true;
+        }
+    }
+}
