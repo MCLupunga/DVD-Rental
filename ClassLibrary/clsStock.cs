a35f06c124afbff3e3d0e499fe5d118c961977f2
Mapalo Lupunga <P16187679@my365.dmu.ac.uk>
2018-03-08 21:41:08 +0000

    Done all tests and updated the class file

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -6,16 +6,119 @@ namespace ClassLibrary
     {
         //private data member from the Stock ID property
         private Int32 mStockID;
+        //private data member from the property
+        private string mName;
+        //private data member from the property
+        private string mGenre;
+        //private data member from the property
+        private string mClassification;
+        //private data member from the property
+        private Int32 mShelfLocation;
+        //private data member from the property
+        private DateTime mReleaseDate;
+        //private data member from the property
+        private Int32 mQuantity;
+        //private data member from the Stock ID property
+        private Int32 mPrice;
 
-        public clsStock()
+        //public property for the classification
+        public string Classification
         {
+            get
+            {
+                //return to the private data
+                return mClassification;
+            }
+            set
+            {
+                //set the value of the private data member
+                mClassification = value;
+            }
         }
-
-        public string Classification { get; set; }
-        public string Genre { get; set; }
-        public string Name { get; set; }
-        public DateTime ReleaseDate { get; set; }
-        public int ShelfLocation { get; set; }
+        //public property for the genre
+        public string Genre
+        {
+            get
+            {
+                //return to the private data
+                return mGenre;
+            }
+            set
+            {
+                //set the value of the private data member
+                mGenre = value;
+            }
+        }
+        //public property for the name
+        public string Name
+        {
+            get
+            {
+                //return to the private data
+                return mName;
+            }
+            set
+            {
+                //set the value of the private data member
+                mName = value;
+            }
+        }
+        //public property for the price
+        public int Price
+        {
+            get
+            {
+                //return to the private data
+                return mPrice;
+            }
+            set
+            {
+                //set the value of the private data member
+                mPrice = value;
+            }
+        }
+        //public property for the quantity
+        public int Quantity
+        {
+            get
+            {
+                //return to the private data
+                return mQuantity;
+            }
+            set
+            {
+                //set the value of the private data member
+                mQuantity = value;
+            }
+        }
+        //public property for the release date
+        public DateTime ReleaseDate
+        {
+            get
+            {
+                //return to the private data
+                return mReleaseDate;
+            }
+            set
+            {
+                //set the value of the private data member
+                mReleaseDate = value;
+            }
+        }
+        //public property for the shelf location
+        public int ShelfLocation
+        {
+            get
+            {
+                //return to the private data
+                return mShelfLocation;
+            }
+            set
+            {
+                //set the value of the private data member
+                mShelfLocation = value;
+            }
+        }
         //public property for the stock ID
         public int StockID
         {
@@ -35,6 +138,13 @@ namespace ClassLibrary
         {
             ///set the private data member to the test data value
             mStockID = 101;
+            mName = "The Hunger Games";
+            mGenre = "Drama";
+            mClassification = "12A";
+            mShelfLocation = 23;
+            mReleaseDate = Convert.ToDateTime(DateTime.Now.Date);
+            mQuantity = 10;
+            mPrice = 2;
             //always return true
             return true;
         }
